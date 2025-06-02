eb191f89d4517fdfe0e79a0b9d825fd509be83ff
ahmadfirdausahla16 <ahmadfirdausyahla@gmail.com>
2025-06-02 22:49:23 +0700

    Add files via upload

---
+++ js/Bab 6/2/2A/script.java
@@ -0,0 +1,11 @@
+Public class Manusia {
+    //definisi
+    Private String nama;
+    //definisi method
+    Public void setNama (String a){
+        nama=a;
+    }
+    Public String getNama(){
+        return nama;
+    }
+}
